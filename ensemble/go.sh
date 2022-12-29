239c7525fdb932c78d9a1f116d0034bf6c91ee81
zuco3 <git@e-west.info>
2022-12-30 02:00:36 +0900

    add auto drive script

---
+++ ensemble/go.sh
@@ -0,0 +1,6 @@
+#jupyter nbconvert --to python ens_augens-re2.ipynb
+python ens_augens-re2.py -l 50
+python ens_augens-re2.py -l 100
+python ens_augens-re2.py -l 200
+python ens_augens-re2.py -l 500
+python ens_augens-re2.py -l 1000
