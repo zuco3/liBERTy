e79152a53e890f119f4f25698d50691f14b76817
zuco3 <git@e-west.info>
2022-12-31 01:18:06 +0900

    add go scripts

---
+++ ensemble/go/gonoaug.sh
@@ -0,0 +1,5 @@
+python ens_augens-re2.py -l 50 -t n
+python ens_augens-re2.py -l 100 -t n
+python ens_augens-re2.py -l 200 -t n
+python ens_augens-re2.py -l 500 -t n
+python ens_augens-re2.py -l 1000 -t n
---
+++ ensemble/go/gonormal.sh
@@ -0,0 +1,10 @@
+python ens_augens-re2.py -l 50
+python ens_augens-re2.py -l 100
+python ens_augens-re2.py -l 200
+python ens_augens-re2.py -l 500
+python ens_augens-re2.py -l 1000
+python ens_augens-re2.py -l 50 -t n
+python ens_augens-re2.py -l 100 -t n
+python ens_augens-re2.py -l 200 -t n
+python ens_augens-re2.py -l 500 -t n
+python ens_augens-re2.py -l 1000 -t n
