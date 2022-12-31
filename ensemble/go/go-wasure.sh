6f2125f62390a96d3234fa45fb6c17b5f063d8c9
zuco3 <git@e-west.info>
2022-12-31 16:48:32 +0900

    add wasure

---
+++ ensemble/go/go-wasure.sh
@@ -0,0 +1,3 @@
+#jupyter nbconvert --to python ens_augens-re2.ipynb
+python ens_augens-re2.py -l 100 -t rids -r 3 -i 3 -d 0.08 -s 2 -a 0
+python ens_augens-re2.py -l 100 -t rids -r 3 -i 3 -d 0.08 -s 2 -a 1
