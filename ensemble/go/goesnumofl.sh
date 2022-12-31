168c3b7ae946f8afb01a457eb82ed48a82fe2162
zuco3 <git@e-west.info>
2022-12-31 14:44:53 +0900

    add slide2 go

---
+++ ensemble/go/goesnumofl.sh
@@ -0,0 +1,21 @@
+#jupyter nbconvert --to python ens_augens-slide2.ipynb
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 50 -t rids
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 100 -t rids
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 200 -t rids
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 500 -t rids
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 1000 -t rids
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 50 -t n
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 100 -t n
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 200 -t n
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 500 -t n
+python ens_augens-slide2.py -r 3 -a 0 -i 3 -d 0.08 -s 2 -l 1000 -t n
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 50 -t rids
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 100 -t rids
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 200 -t rids
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 500 -t rids
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 1000 -t rids
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 50 -t n
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 100 -t n
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 200 -t n
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 500 -t n
+python ens_augens-slide2.py -r 3 -a 1 -i 3 -d 0.08 -s 2 -l 1000 -t n
