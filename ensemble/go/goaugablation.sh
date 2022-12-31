1bf10e44a6852e0ae628a92189c0b70319a96988
zuco3 <git@e-west.info>
2022-12-31 13:55:28 +0900

    add new go

---
+++ ensemble/go/goaugablation.sh
@@ -0,0 +1,8 @@
+python ens_augens-re2.py -a 0 -r 3 -i 3 -d 0.08 -s 2 -l 100 -t ids
+python ens_augens-re2.py -a 0 -r 3 -i 3 -d 0.08 -s 2 -l 100 -t rds
+python ens_augens-re2.py -a 0 -r 3 -i 3 -d 0.08 -s 2 -l 100 -t ris
+python ens_augens-re2.py -a 0 -r 3 -i 3 -d 0.08 -s 2 -l 100 -t rid
+python ens_augens-re2.py -a 1 -r 3 -i 3 -d 0.08 -s 2 -l 100 -t ids
+python ens_augens-re2.py -a 1 -r 3 -i 3 -d 0.08 -s 2 -l 100 -t rds
+python ens_augens-re2.py -a 1 -r 3 -i 3 -d 0.08 -s 2 -l 100 -t ris
+python ens_augens-re2.py -a 1 -r 3 -i 3 -d 0.08 -s 2 -l 100 -t rid
