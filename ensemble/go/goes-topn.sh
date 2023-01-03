7c241368e42372197f1b5efbe02c065ae614f3a3
zuco3 <git@e-west.info>
2023-01-03 11:21:02 +0900

    add goes normal

---
+++ ensemble/go/goes-topn.sh
@@ -0,0 +1,5 @@
+time python ens_augens-slide2.py -l 50 -t n
+time python ens_augens-slide2.py -l 100 -t n
+time python ens_augens-slide2.py -l 200 -t n
+time python ens_augens-slide2.py -l 500 -t n
+time python ens_augens-slide2.py -l 1000 -t n
