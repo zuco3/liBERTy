fba14425383209030460b449088a688b5342e33a
zuco3 <git@e-west.info>
2023-01-04 18:18:01 +0900

    add new scripts

---
+++ ensemble/go/goes-ntop2.sh
@@ -0,0 +1,4 @@
+time python ens_augens-slide2.py -r 1 -d 0.02 -l 50 -t rd
+time python ens_augens-slide2.py -r 1 -d 0.02 -l 100 -t rd
+time python ens_augens-slide2.py -l 50 -t n
+time python ens_augens-slide2.py -l 100 -t n
--- ensemble/go/goes-top3.sh
+++ ensemble/go/goes-top3.sh
@@ -1,5 +1,6 @@
-time python ens_augens-slide2.py -d 0.02 -l 50 -t d
-time python ens_augens-slide2.py -d 0.02 -l 100 -t d
-time python ens_augens-slide2.py -d 0.02 -l 200 -t d
 time python ens_augens-slide2.py -d 0.02 -l 500 -t d
+time python ens_augens-slide2.py -d 0.02 -l 200 -t d
+time python ens_augens-slide2.py -d 0.02 -l 100 -t d
+time python ens_augens-slide2.py -d 0.02 -l 50 -t d
 time python ens_augens-slide2.py -d 0.02 -l 1000 -t d
+time python ens_augens-slide2.py -l 1000 -t n
