d84032ecb332c5bff1e602710bdc88abd4233248
zuco3 <git@e-west.info>
2023-01-04 18:18:01 +0900

    add new scripts

--- ensemble/go/goes-top2.sh
+++ ensemble/go/goes-top2.sh
@@ -1,5 +1,6 @@
-time python ens_augens-slide2.py -r 2 -i 2 -d 0.02 -s 2 -l 50 -t rids
-time python ens_augens-slide2.py -r 2 -i 2 -d 0.02 -s 2 -l 100 -t rids
-time python ens_augens-slide2.py -r 2 -i 2 -d 0.02 -s 2 -l 200 -t rids
 time python ens_augens-slide2.py -r 2 -i 2 -d 0.02 -s 2 -l 500 -t rids
+time python ens_augens-slide2.py -r 2 -i 2 -d 0.02 -s 2 -l 200 -t rids
+time python ens_augens-slide2.py -l 500 -t n
+time python ens_augens-slide2.py -r 2 -i 2 -d 0.02 -s 2 -l 100 -t rids
+time python ens_augens-slide2.py -r 2 -i 2 -d 0.02 -s 2 -l 50 -t rids
 time python ens_augens-slide2.py -r 2 -i 2 -d 0.02 -s 2 -l 1000 -t rids
