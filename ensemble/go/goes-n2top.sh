711c56a6364c5d3d1618612bdef85850fd556ef4
zuco3 <git@e-west.info>
2023-01-06 01:16:10 +0900

    add n2top

---
+++ ensemble/go/goes-n2top.sh
@@ -0,0 +1,6 @@
+#time python ens_augens-slide2.py -l 50 -e 20 -b 64 -t d -d 0.02 -a 0
+time python ens_augens-slide2.py -l 100 -e 20 -b 64 -t rids -r 2 -i 2 -d 0.02 -s 2 -a 0
+time python ens_augens-slide2.py -l 100 -e 20 -b 64 -t d -d 0.02 -a 0
+time python ens_augens-slide2.py -l 100 -e 5 -b 64 -t rids -r 2 -i 2 -d 0.02 -s 2 -a 0
+time python ens_augens-slide2.py -l 100 -e 5 -b 64 -t d -d 0.02 -a 0
+#time python ens_augens-slide2.py -l 200 -e 5 -b 64 -t d -d 0.02 -a 0
