c42b2a39a366b4b940d3648c037d4016cc169aeb
zuco3 <git@e-west.info>
2023-01-13 03:41:35 +0900

    add new goes

---
+++ ensemble/go/go-yarinaoshi.sh
@@ -0,0 +1,9 @@
+time python ens_augens-re2.py -t s -s 1 -a 1
+time python ens_augens-re2.py -t s -s 2 -a 1
+time python ens_augens-re2.py -t s -s 3 -a 1
+time python ens_augens-re2.py -t s -s 4 -a 1
+time python ens_augens-re2.py -t s -s 5 -a 1
+time python ens_augens-re2.py -t s -s 6 -a 1
+time python ens_augens-re2.py -t s -s 7 -a 1
+time python ens_augens-re2.py -t s -s 8 -a 1
+echo "OwattaZe" | mail sotsuron@e-west.info
---
+++ ensemble/go/goes-ntop200.sh
@@ -0,0 +1,4 @@
+time python ens_augens-slide2.py -e 5 -r 2 -i 2 -d 0.02 -s 2 -l 200 -t rids
+time python ens_augens-slide2.py -e 5 -d 0.02 -l 200 -t d
+time python ens_augens-slide2.py -e 5 -r 1 -d 0.02 -l 200 -t rd
+time python ens_augens-slide2.py -e 5 -l 200 -t n
