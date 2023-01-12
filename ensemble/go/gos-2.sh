537ab6102907173624d0633c350b014cc58194dc
zuco3 <git@e-west.info>
2023-01-13 03:54:08 +0900

    add new gos

---
+++ ensemble/go/gos-2.sh
@@ -0,0 +1,13 @@
+time python ens_augens-slide.py -l 50 -a 0 -e 20 -b 64 -t rd -r 2 -d 0.02
+time python ens_augens-slide.py -l 100 -a 0 -e 20 -b 64 -t rd -r 2 -d 0.02
+time python ens_augens-slide.py -l 200 -a 0 -e 20 -b 64 -t rd -r 2 -d 0.02
+time python ens_augens-slide.py -l 500 -a 0 -e 20 -b 64 -t rd -r 2 -d 0.02
+time python ens_augens-slide.py -l 1000 -a 0 -e 20 -b 64 -t rd -r 2 -d 0.02
+
+time python ens_augens-slide.py -l 50 -a 1 -e 20 -b 64 -t rd -r 2 -d 0.02
+time python ens_augens-slide.py -l 100 -a 1 -e 20 -b 64 -t rd -r 2 -d 0.02
+time python ens_augens-slide.py -l 200 -a 1 -e 20 -b 64 -t rd -r 2 -d 0.02
+time python ens_augens-slide.py -l 500 -a 1 -e 20 -b 64 -t rd -r 2 -d 0.02
+time python ens_augens-slide.py -l 1000 -a 1 -e 20 -b 64 -t rd -r 2 -d 0.02
+
+echo "OwattaZe" | mail sotsuron@e-west.info
